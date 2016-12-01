5eabf549e210def8022e1739801493058f670e30
hootnot <f.brekeveld@gmail.com>
2016-12-01 07:27:15 +0100

    price parameter added

--- src/candle-data.py
+++ src/candle-data.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
-"""Retrieve candle data."""
+"""Retrieve candle data.
+
+For complete specs of the endpoint, please check:
+
+    http://developer.oanda.com/rest-live-v20/instrument-ep/
+
+Specs of InstrumentsCandles()
+
+    http://oanda-api-v20.readthedocs.io/en/latest/oandapyV20.endpoints.html
+
+"""
 import argparse
 import json
 from oandapyV20 import API
@@ -8,7 +18,7 @@ import oandapyV20.endpoints.instruments as instruments
 from oandapyV20.definitions.instruments import CandlestickGranularity
 from exampleauth import exampleAuth
 
-
+price = ['M', 'B', 'A', 'BA', 'MBA']
 granularities = CandlestickGranularity().definitions.keys()
 # create the top-level parser
 parser = argparse.ArgumentParser(prog='candle-data')
@@ -16,6 +26,7 @@ parser.add_argument('--nice', action='store_true', help='json indented')
 parser.add_argument('--count', default=0, type=int,
                     help='num recs, if not specified 500')
 parser.add_argument('--granularity', choices=granularities, required=True)
+parser.add_argument('--price', choices=price, default='M', help='Mid/Bid/Ask')
 parser.add_argument('--start', type=str, help="date-time YYYY-MM-DDTHH:MM:SS")
 parser.add_argument('--end', type=str, help="date-time YYYY-MM-DDTHH:MM:SS")
 parser.add_argument('--instruments', type=str, nargs='?',
@@ -40,6 +51,8 @@ class Main(object):
                 params.update({"from": self.clargs.start})
             if self.clargs.end:
                 params.update({"to": self.clargs.end})
+            if self.clargs.price:
+                params.update({"price": self.clargs.price})
             for i in self.clargs.instruments:
                 r = instruments.InstrumentsCandles(instrument=i, params=params)
                 rv = self.api.request(r)
