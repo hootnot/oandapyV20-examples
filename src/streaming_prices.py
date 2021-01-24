e1df70e3f16a3275c8648cf873e3db10425d7fa3
Feite Brekeveld <f.brekeveld@gmail.com>
2021-01-24 16:29:43 +0000

    argparse -> docopt for commandline options parsing

--- src/streaming_prices.py
+++ src/streaming_prices.py
@@ -3,74 +3,81 @@
 
 demonstrate the PricingStream request.
 
-Example:
-    streaming_prices.py --nice --count 10 --inst EUR_USD --inst EUR_JPY
+Usage:
+    streaming_prices.py --instrument <instrument> [--instrument <instrument>] [--nice] [--timeout <timeout>] [--count <count>]
+
+Options:
+    --nice                 json indented formatting
+    --timeout=<timeout>    timeout in seconds
+    --count=<count>        # of records to receive [default: 0] unlimited
 """
-import argparse
 import json
 from oandapyV20 import API
 from oandapyV20.exceptions import V20Error, StreamTerminated
 from oandapyV20.endpoints.pricing import PricingStream
 from exampleauth import exampleAuth
 from requests.exceptions import ConnectionError
-
-# create the top-level parser
-parser = argparse.ArgumentParser(prog='steaming_prices')
-parser.add_argument('--nice', action='store_true', help='json indented')
-parser.add_argument('--timeout', default=0, type=float,
-                    help='timeout in secs., default no timeout')
-parser.add_argument('--count', default=0, type=int,
-                    help='# of records to receive, default = unlimited.')
-parser.add_argument('--instruments', type=str, nargs='?',
-                    action='append', help='instruments')
+import logging
 
 
-accountID, access_token = exampleAuth()
+logging.basicConfig(
+    filename="pricingstream.log",
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(name)s : %(message)s',
+)
 
-# commandline args ...
-clargs = parser.parse_args()
-if not clargs.instruments:
-    parser.parse_args(["--help"])
+logger = logging.getLogger(__name__)
 
-request_params = {}
-if clargs.timeout:
-    request_params = {"timeout": clargs.timeout}
 
-# fetch MAXREC stream records
-MAXREC = clargs.count
+def main(clargs):
+    accountID, access_token = exampleAuth()
 
-api = API(access_token=access_token,
-          environment="practice",
-          request_params=request_params)
+    request_params = {}
+    if clargs['--timeout']:
+        request_params = {"timeout": clargs['--timeout']}
 
-# setup the stream request
-r = PricingStream(accountID=accountID,
-                  params={"instruments": ",".join(clargs.instruments)})
+    # fetch MAXREC stream records
+    MAXREC = int(clargs['--count'])
 
-n = 0
-while True:
-    try:
-        for R in api.request(r):
-            if clargs.nice:
-                R = json.dumps(R, indent=2)
-            print(R)
-            n += 1
-            if MAXREC and n >= MAXREC:
-                r.terminate("maxrecs received: {}".format(MAXREC))
+    api = API(access_token=access_token,
+              environment="practice",
+              request_params=request_params)
 
-    except V20Error as e:
-        # catch API related errors that may occur
-        with open("LOG", "a") as LOG:
-            LOG.write("V20Error: {}\n".format(e))
-        break
-    except ConnectionError as e:
-        with open("LOG", "a") as LOG:
-            LOG.write("Error: {}\n".format(e))
-    except StreamTerminated as e:
-        with open("LOG", "a") as LOG:
-            LOG.write("Stopping: {}\n".format(e))
-        break
-    except Exception as e:
-        with open("LOG", "a") as LOG:
-            LOG.write("??? : {}\n".format(e))
-        break
+    # setup the stream request
+    r = PricingStream(accountID=accountID,
+                      params={"instruments": ",".join(clargs['<instrument>'])})
+
+    n = 0
+    while True:
+        try:
+            for R in api.request(r):
+                if clargs['--nice']:
+                    R = json.dumps(R, indent=2)
+                print(R)
+                n += 1
+                if MAXREC and n >= MAXREC:
+                    r.terminate("maxrecs received: {}".format(MAXREC))
+
+        except V20Error as e:
+            # catch API related errors that may occur
+            logger.error("V20Error: %s", e)
+            break
+
+        except ConnectionError as e:
+            logger.error("%s", e)
+
+        except StreamTerminated as e:
+            logger.error("Stopping: %s", e)
+            break
+
+        except Exception as e:
+            logger.error("%s", e)
+            break
+
+
+if __name__ == '__main__':
+    from docopt import docopt
+
+    # commandline args ...
+    clargs = docopt(__doc__)
+    main(clargs)
